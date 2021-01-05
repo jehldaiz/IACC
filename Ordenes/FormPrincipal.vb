07cb6076722fedfd3759441c2b963435332136af
jehldaiz <76973426+jehldaiz@users.noreply.github.com>
2021-01-04 22:49:37 -0400

    Prueba para IACC

--- Ordenes/FormPrincipal.vb
+++ Ordenes/FormPrincipal.vb
@@ -36,9 +36,13 @@ Public Class FormPrincipal
             FormBuscarOT.Show()
         End If
 
-        If (Login.WobjUserSession.IdPerfil = 11 Or Login.WobjUserSession.NombrePerfil = "COORDINADOR") Then
-            FormCoord.Show()
-        End If
+
+        ''Comentario para ver la actualizacion en el repositorio GitHub
+        ''If (Login.WobjUserSession.IdPerfil = 11 Or Login.WobjUserSession.NombrePerfil = "COORDINADOR") Then
+        ''FormCoord.Show()
+        ''End If
+
+
     End Sub
 
     Private Sub SalirToolStripMenuItem_Click(sender As System.Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
